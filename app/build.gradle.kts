935bba97a417d0e2492f531c7e67d36303211e91
pavel <pavel.lukyanovv@gmail.com>
2024-02-29 22:23:42 +0300

    build: Readme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.pavellukyanov.themartian"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11001
+        versionCode = 11000
         versionName = "1.1"
 
         archivesName = "${rootProject.name}-$versionName"
