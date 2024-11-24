90ad006759c4aff521ef374baa778d0ab25d7e05
pavel <pavel.lukyanovv@gmail.com>
2024-11-24 22:13:23 +0300

    refactor: any logic

--- app/src/main/java/com/pavellukyanov/themartian/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/pavellukyanov/themartian/ui/screens/splash/SplashScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,10 +14,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.pavellukyanov.themartian.R
-import com.pavellukyanov.themartian.ui.screens.gallery.GalleryState
 import com.pavellukyanov.themartian.utils.ext.Launch
-import com.pavellukyanov.themartian.utils.ext.asState
-import com.pavellukyanov.themartian.utils.ext.receive
 import com.pavellukyanov.themartian.utils.ext.subscribeEffect
 import org.koin.androidx.compose.koinViewModel
 
@@ -28,8 +24,6 @@ fun SplashScreen(
     navController: NavHostController,
     reducer: SplashReducer = koinViewModel()
 ) {
-    val state by reducer.asState()
-
     Launch {
         reducer.dispatch(SplashAction)
         reducer.subscribeEffect { effect ->
@@ -37,20 +31,16 @@ fun SplashScreen(
         }
     }
 
-    state.receive<GalleryState>(
-        content = { _ ->
-            Box(
-                modifier = modifier
-                    .background(Color.DarkGray)
-                    .fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Image(
-                    modifier = Modifier.size(110.dp),
-                    painter = painterResource(id = R.drawable.ic_mars),
-                    contentDescription = stringResource(id = R.string.default_image_description)
-                )
-            }
-        }
-    )
+    Box(
+        modifier = modifier
+            .background(Color.DarkGray)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.size(110.dp),
+            painter = painterResource(id = R.drawable.ic_mars),
+            contentDescription = stringResource(id = R.string.default_image_description)
+        )
+    }
 }
